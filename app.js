ddf02d79a360c32bd03c4e5f9f55dfa8e36fc98d
lfabrizio <louisfabrizio.student@careerdevs.com>
2019-12-17 21:03:01 -0500

    events module

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
-// always use async
-// const files = fs.readdirSync('./');
-
-// console.log(files);
-
-fs.readdir('./', function(err, files){
-    if(err) console.log('Error', err);
-    else console.log('Result', files);
+const EventEmitter = require('events');
+// class human object John 
+const emitter = new EventEmitter();
+// raise an event
+// register a listener 
+emitter.on('messageLogged', function() {
+    console.log('Listener called');
 });
+emitter.emit('messageLogged');
+// emit means making a noise or signalling 
