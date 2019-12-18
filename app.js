35732e3bfe811f04dc60783fc2d5a3f9ce8bbe8b
lfabrizio <louisfabrizio.student@careerdevs.com>
2019-12-17 20:53:09 -0500

    updated

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const fs = require('fs');
 // always use async
-const files = fs.readdirSync('./');
+// const files = fs.readdirSync('./');
 
-console.log(files);
+// console.log(files);
+
+fs.readdir('./', function(err, files){
+    if(err) console.log('Error', err);
+    else console.log('Result', files);
+});
