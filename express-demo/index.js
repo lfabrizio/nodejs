8437d25fac969aee52746deede74734af988beba
lfabrizio <louisfabrizio.student@careerdevs.com>
2019-12-22 18:50:46 -0500

    folder4done

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
@@ -17,6 +18,10 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+        
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -24,11 +29,42 @@ app.post('/api/courses', (req, res) => {
     courses.push(course);
     res.send(course);
 })
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('course w given idea not found');
+    
+    const { error } = validateCourse(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+        
+    
+    course.name = req.body.name;
+    res.send(course);
+});
 
+
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('course w given idea not found');
+    
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);    
+}
 
 app.get('/api/courses/:id', (req, res) => {
 const course = courses.find(c => c.id === parseInt(req.params.id));
-if (!course) res.status(404).send('course w given idea not found');
+if (!course) return res.status(404).send('course w given idea not found');
 res.send(course);
 });
 
