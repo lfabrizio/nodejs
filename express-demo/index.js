afd79b80299195d139ee48f8ae592af526a3d6e1
lfabrizio <louisfabrizio.student@careerdevs.com>
2019-12-26 17:50:57 -0500

    environements

--- express-demo/index.js
+++ express-demo/index.js
@@ -5,11 +5,17 @@ const logger = require('./logger');
 const express = require('express');
 const app = express();
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extened: true }));
 app.use(express.static('public'));
 app.use(helmet());
-app.use(morgan('tiny'));
+
+if (app.get('env') === 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled...');
+}
+
 app.use(logger);
 
 app.use(function(req, res, next) {
