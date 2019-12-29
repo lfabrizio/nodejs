6862d0bb16011cde0e15a790210947fc9854f345
lfabrizio <louisfabrizio.student@careerdevs.com>
2019-12-29 18:55:43 -0500

    folder6 done

--- async-demo/index.js
+++ async-demo/index.js
@@ -6,12 +6,24 @@ getUser(1, (user) => {
         })
     })
 });
-
+//promise based approach
 getUser(1)
 .then(user => getRepositories(user.gitHubUsername))
 .then(repos => getCommits(repos[0]))
 .then(commits => console.log('Commits', commits))
 .catch(err => console.log('Error', err.message));
+
+// async and await
+async function displayCommits() {
+    try {
+const user = await getUser(1);
+const repos = await getRepositories(user.gitHubUsername);
+const commits = await getCommits(repos[0]);
+console.log(commits);
+    }
+
+}
+displayCommits();
 
 console.log('After');
 
