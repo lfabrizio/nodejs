739471f9c5d59cd7675156f519587b193388a986
lfabrizio <louisfabrizio.student@careerdevs.com>
2019-12-29 18:36:44 -0500

    running par

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -1,2 +1,16 @@
-const p = Promise.reject(new Error('reason for rejection...'));
-p.catch(error => console.log(error));
+const p1 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Async operation 1 ...');
+        resolve(1);
+    }, 2000);
+});
+
+const p2 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Async operation 2 ...');
+        resolve(2);
+    }, 2000);
+});
+Promise.race([p1, p2])
+.then(result => console.log(result))
+.catch(err => console.log('Error', err.message));
