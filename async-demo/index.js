db24d68ac19cd2d47bd6aef9a8fcf0c375ae0a22
lfabrizio <louisfabrizio.student@careerdevs.com>
2019-12-29 11:43:13 -0500

    patterns dealing w async code

---
+++ async-demo/index.js
@@ -0,0 +1,12 @@
+console.log('Before');
+const user = getUser(1);
+console.log(user);
+console.log('After');
+
+function getUser(id) {
+    setTimeout(() => {
+        console.log('Reading a user from a database..');
+        return { id: id, gitHubUsername: 'mosh' }
+        }, 2000);
+        return 1;
+}
