a3bce31b25b259a08c19c6e8806d531af02e3a00
lfabrizio <louisfabrizio.student@careerdevs.com>
2020-01-01 17:41:05 -0500

    10.8

--- vidly/index.js
+++ vidly/index.js
@@ -1,6 +1,13 @@
+const config = require('config');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectedid')(Joi);
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
 
@@ -11,6 +18,10 @@ mongoose.connect('mongodb://localhost/vidly')
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('./api/movies', movies);
+app.use('./api/rentals', rentals);
+app.use('./api/users', users);
+app.use('./api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
